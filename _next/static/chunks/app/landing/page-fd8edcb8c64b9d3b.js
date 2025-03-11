9265cd86ce4b1e3903244c3c62a6e6dd564b748a
TURKI ALGHAMDI <kiingtrkii@gmail.com>
2025-03-12 01:35:03 +0300

    Updates

--- _next/static/chunks/app/landing/page-fd8edcb8c64b9d3b.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[18],{456:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,8173,23)),Promise.resolve().then(n.t.bind(n,3272,23))},3272:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[563,173,441,517,358],()=>s(456)),_N_E=e.O()}]);
